956079016980f1c6875a5c1cc611b29f13a32ee1
minprogramador <minprogramador@gmail.com>
2021-03-29 20:31:23 -0300

    estruturas de repeticao

---
+++ estruturas_de_repeticao.js
@@ -0,0 +1,23 @@
+
+//for
+for(var i = 0; i <= 10; i++)
+{
+	console.log(i);
+}
+
+//while
+var numero = 0;
+
+while(numero <= 10)
+{
+	console.log(numero);
+	numero++;
+}
+
+
+//do-while
+var cont = 0;
+do{
+	console.log('imprime o valor de cont:' + cont);
+	cont++;
+}while(cont <= 5);
