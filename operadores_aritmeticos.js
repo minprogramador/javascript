f80e77500f6daf08940acbc74e70788dbe8d6ceb
minprogramador <minprogramador@gmail.com>
2021-03-29 20:55:22 -0300

    operadores aritmeticos

---
+++ operadores_aritmeticos.js
@@ -0,0 +1,56 @@
+
+
+// Operadores aritmeticos
+
+/*
+
++ Adicao
+- Subtracao
+* Multiplicacao
+/ divisao
+
+//Operadores aritmeticos abreviados
++=
+-=
+*=
+/=
+
+*/
+
+console.log(7 / 0); //infinity
+
+//21
+console.log("42" / 2);
+
+console.log("Geek" * 3); // NaN not a number.
+
+console.log(0.1 + 0.7); //0.7999999999999999
+
+var num = 42;
+console.log(num.toString());
+
+console.log(13.343455544.toFixed(2)); //fixa casas decimais..
+
+console.log(Math.PI);
+
+console.log(Math.pow(3, 2));
+console.log(Math.sqrt(81));
+
+console.log(typeof(42));
+console.log(typeof("Geek"));
+
+console.log(Number.isInteger(45));
+
+console.log(Number.isInteger("Gekk"));
+
+//conversao binaria
+var valor = 10;
+console.log(valor.toString(2));
+
+
+
+
+
+
+
+
