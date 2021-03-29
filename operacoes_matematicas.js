a381fd1bca9f43a17f1f73b881a6c5cdfdda09d7
minprogramador <minprogramador@gmail.com>
2021-03-29 20:25:06 -0300

    operacoes matematicas

---
+++ operacoes_matematicas.js
@@ -0,0 +1,42 @@
+var num1 = 42, num2 = 19, nome = 'Geek';
+//var num2= 19;
+
+//soma
+
+var res = num1 + num2;
+console.log("O resultado é: " + res);
+console.log(nome);
+
+//CUIDADE dica.
+var num3 = "39";
+res = num1 + num3; // concatenou por ter aspas duplas em num3.
+console.log(res);
+
+//subtracao
+res = num1 - num2;
+console.log(res);
+
+//Multiplicacao
+res = num1 * num2;
+console.log(res);
+
+//divisao
+res = num1 / 2;
+console.log(res);
+
+//Modulo (resto da divisao)
+res = num2 % 2;
+console.log(res);
+
+//incremento/decremento
+num1 = num1 + 8;
+console.log(num1);
+num1 +=8;
+console.log(num1);
+console.log(num1++); //imprime a variavel e depois incrementa 50, 51
+console.log(num1--); //imprime a variavel e depois decrementa 51, 50
+console.log(++num1); //incrementa e depois imprime.
+console.log(--num1); //decrementa e depois imprime.
+
+
+
