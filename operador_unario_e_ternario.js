32b9a066186cb16f5dd2d3c2400efba76030162c
minprogramador <minprogramador@gmail.com>
2021-03-29 20:58:00 -0300

    operador unario e ternario

---
+++ operador_unario_e_ternario.js
@@ -0,0 +1,22 @@
+
+
+//Operador unario e operacao ternaria
+
+//Unario
+var idade = 25;
+idade ++ //operacao unaria
+
+console.log(idade);
+
+//ternario
+
+var sexo = 'f';
+
+if(sexo === 'm'){
+	console.log('Masculino');
+}else{
+	console.log('Feminino');
+}
+
+//operador ternario
+console.log(sexo === 'm' ? 'Masculino' : 'Feminino');
