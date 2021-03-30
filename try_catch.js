258cab5f5b163e12f67c2651a524d33454ffb863
minprogramador <minprogramador@gmail.com>
2021-03-29 23:21:45 -0300

    mais try catch

--- try_catch.js
+++ try_catch.js
@@ -7,3 +7,14 @@ try{
 }
 
 console.log('oi ....');
+
+function dividir(num1, num2){
+	if(num1 === 0 || num2 === 0){
+		throw("Os valores devem ser positivos");
+	}else{
+		return num1 / num2;
+	}
+}
+
+let ret = dividir(8, 2);
+console.log(ret);
