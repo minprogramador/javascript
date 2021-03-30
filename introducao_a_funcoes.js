64ac7a3cdf7f88792f21d08ca9c8489b40385a5f
minprogramador <minprogramador@gmail.com>
2021-03-29 21:00:14 -0300

    introducao a funcoes

---
+++ introducao_a_funcoes.js
@@ -0,0 +1,24 @@
+
+
+//intro as funcoes
+
+function somar(num1, num2) {
+	console.log(num1 + num2);
+}
+
+function mensagem(){
+	console.log('Esta é minha mensagem');
+}
+
+function multiplicar(n1, n2){
+	return n1 * n2;
+}
+
+somar(6, 4);
+
+mensagem();
+
+var r = multiplicar(3, 9);
+console.log(r);
+
+console.log(multiplicar(234, 123));
